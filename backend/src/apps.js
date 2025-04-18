cf1150b05ca3f47274361b9f402409be873646e6
Nicolas-Asafe <nicolasasafe45@gmail.com>
2025-04-17 21:01:55 -0300

    feat: validação para usar API

--- backend/src/apps.js
+++ backend/src/apps.js
@@ -4,8 +4,20 @@ import cors from 'cors';
 import UserRouter from './routers/user.router.js';
 import PostRouter from '../src/routers/post.router.js'
 import { ConnectDB } from './utils/MongooseHandler.js';
+import env from 'dotenv'
+env.config();
 const app = Express();
 
+
+
+app.use((req, res, next) => {
+    const apiKey = req.headers['x-api-key'];
+    if (apiKey !== process.env.SECRET_KEY_ACCESS) {
+      return res.status(403).json({ message: "you can't access this api" });
+    }
+    next();
+  });
+  
 app.use(cors());
 app.use(Express.json());
 ConnectDB()
